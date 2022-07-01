a58d56c6fe04e16425026a20d0a0c90f6d932bdd
lgk-bsw <lgk@patorg.de>
2022-07-01 09:11:55 +0200

    Moved Storybook output to ./docs

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,11 @@
 import { addons } from "@storybook/addons"
 import BlueTheme from "./BlueTheme"
+import favicon from "./assets/icon.ico"
+
+const link = document.createElement("link")
+link.setAttribute("rel", "shortcut icon")
+link.setAttribute("href", favicon)
+document.head.appendChild(link)
 
 addons.setConfig({
     theme: BlueTheme
